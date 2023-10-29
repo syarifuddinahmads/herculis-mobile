d2f3c46a07c8b6f64be99bc6bcd374aa432f3e65
Ahmad Syarifuddin <syarifuddinahmads.me@gmail.com>
2023-10-29 21:53:54 +0700

    lupa password

--- lib/views/auth/forgot_password_view.dart
+++ lib/views/auth/forgot_password_view.dart
@@ -1,38 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:herculismobile/widgets/button_widget.dart';
+import 'package:herculismobile/widgets/textfield_widget.dart';
 
 class ForgotPasswordView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lupa Kata Sandi'),
+        title: const Text('Lupa Kata Sandi'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Text(
-              'Lupa kata sandi? Masukkan alamat email Anda di bawah ini, dan kami akan mengirimkan instruksi pemulihan kata sandi ke email Anda.',
+            const Text(
+              'Masukkan alamat email Anda di bawah ini, dan kami akan mengirimkan instruksi pemulihan kata sandi ke email Anda.',
               style: TextStyle(fontSize: 16.0),
             ),
             SizedBox(height: 16.0),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                icon: Icon(Icons.email),
-              ),
-            ),
+            TextFieldWidget(
+                labelText: "Email", controller: TextEditingController()),
             SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                // Implementasi logika pemulihan kata sandi di sini
-              },
-              child: Text('Kirim Instruksi Pemulihan'),
-            ),
+            ButtonWidget(onPressed: () {}, text: 'Submit'),
           ],
         ),
       ),
